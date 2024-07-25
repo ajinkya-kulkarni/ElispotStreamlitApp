2100c9e931ebf1d8e99b3793d1c4f68d67f8d7fd
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2024-07-25 10:47:57 +0200

    Update PyElispotAnalysis_StreamlitApp_DL.py

--- PyElispotAnalysis_StreamlitApp_DL.py
+++ PyElispotAnalysis_StreamlitApp_DL.py
@@ -156,15 +156,14 @@ plt.axis("off")
 # Adjust layout
 plt.tight_layout()
 
+##########################################################################
+
 # Save the figure to a bytes buffer
 buf = BytesIO()
 plt.savefig(buf, format='png', bbox_inches='tight', dpi = 300, pad_inches=0)
 buf.seek(0)
-
 # Load the image from the buffer
 pil_img = Image.open(buf)
-
-##########################################################################
 
 buf = BytesIO()
 pil_img.save(buf, format="TIFF")
@@ -177,54 +176,40 @@ buf.close()
 
 ##########################################################################
 
-# Setup figure and axes for a 1x4 grid
-fig, axs = plt.subplots(1, 3, figsize=(12, 4), dpi=300)
+# # Setup figure and axes for a 1x4 grid
+# fig, axs = plt.subplots(1, 3, figsize=(12, 4), dpi=300)
 
-# First subplot: Just the original image
-axs[0].imshow(display_image, cmap="gray")
-axs[0].axis("off")
-axs[0].set_title('Image', pad = 10)
+# # First subplot: Just the original image
+# axs[0].imshow(display_image, cmap="gray")
+# axs[0].axis("off")
+# axs[0].set_title('Image', pad = 10)
 
-# Third subplot: Original image with scatter points colored by intensity
-axs[1].imshow(display_image, cmap="gray")
-scatter = axs[1].scatter(x_coords, y_coords, c=intensity_image, s=10, cmap=custom_cmap, 
-linewidth=0.5, edgecolors='black', alpha=0.5)
-axs[1].axis("off")
-axs[1].set_title(f'Prediction, {len(y_coords)} Spots', pad = 10)
+# # Third subplot: Original image with scatter points colored by intensity
+# axs[1].imshow(display_image, cmap="gray")
+# scatter = axs[1].scatter(x_coords, y_coords, c=intensity_image, s=10, cmap=custom_cmap, 
+# linewidth=0.5, edgecolors='black', alpha=0.5)
+# axs[1].axis("off")
+# axs[1].set_title(f'Prediction, {len(y_coords)} Spots', pad = 10)
 
-# Fourth subplot: Normalized histogram of spot intensities
-n, bins, patches = axs[2].hist(intensity_image, bins=10, color='tab:blue', density=True, 
-alpha=0.5, rwidth=0.5)
-axs[2].set_title('Normalized Spot Intensities', pad = 10)
-axs[2].set_xlabel('Normalized Intensity')
-axs[2].set_ylabel('Frequency')
-axs[2].grid(which='both', axis='y', alpha=0.1)
-axs[2].grid(which='both', axis='x', alpha=0.1)
-axs[2].set_ylim(0, np.ceil(max(n) * 10) / 10 + 0.5)
-axs[2].set_xlim(-0.02, 1.02)
+# # Fourth subplot: Normalized histogram of spot intensities
+# n, bins, patches = axs[2].hist(intensity_image, bins=10, color='tab:blue', density=True, 
+# alpha=0.5, rwidth=0.5)
+# axs[2].set_title('Normalized Spot Intensities', pad = 10)
+# axs[2].set_xlabel('Normalized Intensity')
+# axs[2].set_ylabel('Frequency')
+# axs[2].grid(which='both', axis='y', alpha=0.1)
+# axs[2].grid(which='both', axis='x', alpha=0.1)
+# axs[2].set_ylim(0, np.ceil(max(n) * 10) / 10 + 0.5)
+# axs[2].set_xlim(-0.02, 1.02)
 
-# Adjust layout
-plt.tight_layout()
+# # Adjust layout
+# plt.tight_layout()
 
 # st.pyplot(fig)
 
 ##########################################################################
 
-buf.seek(0)
 
-# Load the image from the buffer
-pil_img = Image.open(buf)
-
-##########################################################################
-
-buf = BytesIO()
-pil_img.save(buf, format="TIFF")
-byte_im = buf.getvalue()
-
-btn = st.download_button(label="Download Intensity Image", data=byte_im, file_name="Result2.tif")
-
-# Close the buffer
-buf.close()
 
 ##########################################################################
 
