067c147218234b8dcb210e7840ef95dcf6c4ce85
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2024-07-29 12:53:04 +0200

    Update PyElispotAnalysis_StreamlitApp_DL.py

--- PyElispotAnalysis_StreamlitApp_DL.py
+++ PyElispotAnalysis_StreamlitApp_DL.py
@@ -9,16 +9,6 @@ import pandas as pd
 from spotiflow.model import Spotiflow
 spotiflow_model = Spotiflow.from_folder("Pre_Trained_Model")
 
-import sys
-# Don't generate the __pycache__ folder locally
-sys.dont_write_bytecode = True 
-# Print exception without the buit-in python warning
-sys.tracebacklimit = 0 
-
-##########################################################################
-
-from modules import *
-
 allowed_image_size = 1000 # Only images with sizes less than 1000x1000 allowed
 
 ##########################################################################
@@ -39,7 +29,7 @@ st.set_page_config(
 	menu_items={
 		'Get help': 'mailto:kulkajinkya@gmail.com',
 		'Report a bug': 'mailto:kulkajinkya@gmail.com',
-		'About': 'This is an application for demonstrating the PyElispotAnalysis package. Developed, tested, and maintained by Ajinkya Kulkarni: https://github.com/ajinkya-kulkarni.'
+		'About': 'Developed, tested, and maintained by Ajinkya Kulkarni: https://github.com/ajinkya-kulkarni'
 	}
 )
 
@@ -48,7 +38,8 @@ st.set_page_config(
 # Set the title of the web app
 st.title(':blue[Deep learning based spot detection]')
 
-st.caption('Sample image to test this application is available [here](https://github.com/ajinkya-kulkarni/ElispotStreamlitApp/blob/main/image.tif).', unsafe_allow_html = False)
+st.caption('Sample image to test this application is available [here](https://github.com/ajinkya-kulkarni/ElispotStreamlitApp/blob/main/image.tif).', 
+	   unsafe_allow_html = False)
 
 ##########################################################################
 
@@ -56,14 +47,15 @@ st.caption('Sample image to test this application is available [here](https://gi
 st.markdown(':blue[Upload the image to be analyzed.]')
 
 # Add a file uploader to allow the user to upload an image file
-uploaded_file = st.file_uploader("Upload a file", type = ["tif", "tiff", "png", "jpg", "jpeg"], accept_multiple_files = False, label_visibility = 'collapsed')
+uploaded_file = st.file_uploader("Upload a file", type = ["tif", "tiff", "png", "jpg", "jpeg"], 
+				 accept_multiple_files = False, label_visibility = 'collapsed')
 
 ######################################################################
 
 # If no file was uploaded, stop processing and exit early
 if uploaded_file is None:
 	st.stop()
-
+	
 ##############################################################
 
 original_image_pil = Image.open(uploaded_file)
@@ -76,18 +68,38 @@ if original_image_grayscale_np.shape[0] > allowed_image_size or original_image_g
 	st.error('Uploaded image exceeds the allowed image size. Please reduce the image size to 1000x1000.')
 	st.stop()
 
+def normalize_image_percentile(image, small_diff_threshold=1e-4):
+    # Convert image to float32 for precise division
+    image = image.astype(np.float32)
+
+    # Calculate the 1st and 99th percentiles
+    p1 = np.percentile(image, 1)
+    p99 = np.percentile(image, 99)
+
+    # Check if the difference is too small or zero
+    if p99 - p1 < small_diff_threshold:
+        normalized_image = np.full(image.shape, 0)
+    else:
+        # Normalize the image
+        normalized_image = (image - p1) / (p99 - p1)
+        # Clip values to [0, 1]
+        normalized_image = np.clip(normalized_image, 0, 1)
+
+    return normalized_image
+
 normalized_original_image_grayscale_np = normalize_image_percentile(original_image_grayscale_np)
 
 inverted_image_array_np = 1 - normalized_original_image_grayscale_np
 
-points, details = spotiflow_model.predict(inverted_image_array_np, exclude_border=False, verbose=False, device="cpu", subpix=True)
+##############################################################
+
+points, details = spotiflow_model.predict(inverted_image_array_np, exclude_border=False, 
+					  verbose=False, device="auto", subpix=True)
 
 ##############################################################
 
 x_coords = points[:, 1].astype(int)
 y_coords = points[:, 0].astype(int)
-
-assert len(x_coords) == len(y_coords), "The lengths of x_coords and y_coords are not the same."
 
 # Combine x, y coordinates, and intensities directly
 spot_points = np.column_stack((x_coords, y_coords, details.intens.flatten()))
@@ -97,8 +109,10 @@ intensities = spot_points[:, 2]
 
 ##############################################################
 
-# Min-max normalization
-def min_max_normalize(intensities):
+def min_max_normalize_intensities(intensities):
+    if intensities.size == 0:
+        return np.array([])  # Return an empty array if intensities is empty
+    
     min_value = np.min(intensities)
     max_value = np.max(intensities)
     
@@ -109,8 +123,8 @@ def min_max_normalize(intensities):
         normalized_intensities = (intensities - min_value) / (max_value - min_value)
     
     return normalized_intensities
-
-normalized_intensities = min_max_normalize(intensities)
+    
+normalized_intensities = min_max_normalize_intensities(intensities)
 
 ##############################################################
 
@@ -120,28 +134,52 @@ DPI = 200
 
 ##############################################################
 
-# Create the figure with the specified size and DPI
-fig = plt.figure(figsize=(4, 4), dpi = DPI)
+if points.size != 0:
+    # Create the figure with the specified size and DPI
+    fig = plt.figure(figsize=(4, 4), dpi=DPI)
 
-# Plot the image and scatter plot
-plt.imshow(display_image, cmap="gray")
-scatter = plt.scatter(x_coords, y_coords, s=20, linewidth=1, edgecolors='black', facecolors = "yellow", alpha=0.5)
-plt.title(f'{len(x_coords)} spots detected')
-plt.axis("off")
+    # Plot the image and scatter plot
+    plt.imshow(display_image, cmap="gray")
+    scatter = plt.scatter(x_coords, y_coords, s=20, linewidth=1, edgecolors='black', facecolors="yellow", alpha=0.5)
+    plt.title(f'{len(x_coords)} spots detected')
+    plt.axis("off")
 
-# Adjust layout
-plt.tight_layout()
+    # Adjust layout
+    plt.tight_layout()
 
-col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.empty()
-	
-with col2:
-    st.pyplot(fig)
+    with col1:
+        st.empty()
+    
+    with col2:
+        st.pyplot(fig)
 
-with col3:
-    st.empty()
+    with col3:
+        st.empty()
+
+else:
+    # Create the figure with the specified size and DPI without scatter plot
+    fig = plt.figure(figsize=(4, 4), dpi=DPI)
+
+    # Plot the image without scatter plot
+    plt.imshow(display_image, cmap="gray")
+    plt.title('No spots detected')
+    plt.axis("off")
+
+    # Adjust layout
+    plt.tight_layout()
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.empty()
+    
+    with col2:
+        st.pyplot(fig)
+
+    with col3:
+        st.empty()
 
 ##########################################################################
 
@@ -167,7 +205,8 @@ st.divider()
 
 ##########################################################################
 
-# Setup figure and axes for a 1x4 grid
+
+# Setup figure and axes for a 1x3 grid
 fig, axs = plt.subplots(1, 3, figsize=(12, 4), dpi=DPI)
 
 # First subplot: Just the original image
@@ -175,28 +214,36 @@ axs[0].imshow(display_image, cmap="gray")
 axs[0].axis("off")
 axs[0].set_title('Image')
 
-# Third subplot: Original image with scatter points colored by intensity
-axs[1].imshow(display_image, cmap="gray")
-scatter = axs[1].scatter(x_coords, y_coords, s=20, linewidth=0.5, edgecolors='black', c=normalized_intensities, 
-			 cmap='coolwarm', alpha=0.5)
-axs[1].axis("off")
-axs[1].set_title('Spot intensities')
+if points.size != 0:
+    # Second subplot: Original image with scatter points colored by intensity
+    axs[1].imshow(display_image, cmap="gray")
+    scatter = axs[1].scatter(x_coords, y_coords, s=20, linewidth=0.5, edgecolors='black', c=normalized_intensities, 
+                             cmap='coolwarm', alpha=0.5)
+    axs[1].axis("off")
+    axs[1].set_title('Spot intensities')
 
-# # Add colorbar to the scatter plot
-# divider = make_axes_locatable(axs[1])
-# cax = divider.append_axes("right", size="3%", pad=0.07)
-# cbar = fig.colorbar(scatter, cax=cax)
-# cbar.set_label('Normalized Spot Intensity')
+    # Add colorbar to the scatter plot
+    divider = make_axes_locatable(axs[1])
+    cax = divider.append_axes("right", size="3%", pad=0.07)
+    cbar = fig.colorbar(scatter, cax=cax)
+    cbar.set_label('Normalized Spot Intensity')
 
-# Fourth subplot: Normalized histogram of spot intensities
-n, bins, patches = axs[2].hist(normalized_intensities, bins=10, color='tab:blue', density=True, alpha=0.5, rwidth=0.5)
-axs[2].set_title('Normalized Spot Intensities')
-axs[2].set_xlabel('Normalized Intensity')
-axs[2].set_ylabel('Frequency')
-axs[2].grid(which='both', axis='y', alpha=0.1)
-axs[2].grid(which='both', axis='x', alpha=0.1)
-axs[2].set_ylim(0, np.ceil(max(n) * 10) / 10 + 0.5)
-axs[2].set_xlim(-0.01, 1.01)
+    # Third subplot: Normalized histogram of spot intensities
+    n, bins, patches = axs[2].hist(normalized_intensities, bins=10, color='tab:blue', density=True, alpha=0.5, rwidth=0.5)
+    axs[2].set_title('Normalized Spot Intensities')
+    axs[2].set_xlabel('Normalized Intensity')
+    axs[2].set_ylabel('Frequency')
+    axs[2].grid(which='both', axis='y', alpha=0.1)
+    axs[2].grid(which='both', axis='x', alpha=0.1)
+    axs[2].set_ylim(0, np.ceil(max(n) * 10) / 10 + 0.5)
+    axs[2].set_xlim(-0.01, 1.01)
+else:
+    # If no points, hide the second and third subplots
+    axs[1].axis("off")
+    axs[1].set_title('No spots detected')
+    
+    axs[2].axis("off")
+    axs[2].set_title('No data')
 
 # Adjust layout
 plt.tight_layout()
@@ -228,16 +275,28 @@ st.divider()
 ##########################################################################
 
 def create_intensity_stats(normalized_intensities):
-    stats = {
-        "Spots": len(normalized_intensities),
-        "Intensity Min": np.min(normalized_intensities),
-        "Intensity Max": np.max(normalized_intensities),
-        "Intensity Mean": np.mean(normalized_intensities),
-        "Intensity Median": np.median(normalized_intensities),
-        "Intensity Std Dev": np.std(normalized_intensities),
-        "Intensity Variance": np.var(normalized_intensities),
-        "Intensity Mode": pd.Series(normalized_intensities).mode().values[0] if not pd.Series(normalized_intensities).mode().empty else np.nan
-    }
+    if len(normalized_intensities) == 0:
+        stats = {
+            "Spots": 0,
+            "Intensity Min": 0,
+            "Intensity Max": 0,
+            "Intensity Mean": 0,
+            "Intensity Median": 0,
+            "Intensity Std Dev": 0,
+            "Intensity Variance": 0,
+            "Intensity Mode": 0
+        }
+    else:
+        stats = {
+            "Spots": len(normalized_intensities),
+            "Intensity Min": np.min(normalized_intensities),
+            "Intensity Max": np.max(normalized_intensities),
+            "Intensity Mean": np.mean(normalized_intensities),
+            "Intensity Median": np.median(normalized_intensities),
+            "Intensity Std Dev": np.std(normalized_intensities),
+            "Intensity Variance": np.var(normalized_intensities),
+            "Intensity Mode": pd.Series(normalized_intensities).mode().values[0] if not pd.Series(normalized_intensities).mode().empty else np.nan
+        }
     
     return pd.DataFrame([stats])
 
